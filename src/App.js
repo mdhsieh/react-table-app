42b53e456bb567216fa4a737b55f5b6ea1c70e9f
Michael Hsieh <mdhsieh8@gmail.com>
2021-09-03 22:13:54 -0700

    feat: Add form and submit button to add a new character.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
 import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
     state = {
         characters: [
-            {
+            /*{
                 name: "Charlie",
                 job: "Janitor" 
             },
@@ -19,10 +20,11 @@ class App extends Component {
             {
                 name: "Dennis",
                 job: "Bartender" 
-            }
+            }*/
         ]
     }
 
+    /** Remove a character from the table when button clicked*/
     removeCharacter = (index) => {
         const {characters} = this.state
 
@@ -33,11 +35,21 @@ class App extends Component {
         })
     }
 
+    handleSubmit = (character) => {
+        this.setState({characters: [...this.state.characters, character]})
+    }
+
     render() {
         const {characters} = this.state
       return(
         <div className="container">
-          <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+            <p>Add a character with a name and a job to the table</p>
+            <Table 
+                characterData={characters} 
+                removeCharacter={this.removeCharacter}
+            />
+            <h3>Add New</h3>
+            <Form handleSubmit={this.handleSubmit}/>
         </div>
       )
     }
